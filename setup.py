6a1e15e082f8fc96adcfe4bc305d972f83467119
Sergey Lisov <sleirsgoevy@gmail.com>
2022-04-10 18:02:02 +0300

    PEP-0440

--- setup.py
+++ setup.py
@@ -1,20 +1,8 @@
 import setuptools
 
-def get_version():
-    import os
-    try:
-        if os.path.isfile('.git/HEAD'):
-            with open('.git/HEAD') as file:
-                ref = [i for i in file if i.startswith('ref: ')][0][5:].strip()
-            if os.path.isfile('.git/'+ref):
-               with open('.git/'+ref) as file:
-                   return 'git-%s' % file.read(6)
-    except: pass
-    return 'unknown'
-
 setuptools.setup(
     name = 'brutejudge',
-    version = get_version(),
+    version = '0.2022.4.10',
     packages = setuptools.find_packages(),
     entry_points = {
         'console_scripts': ['brutejudge = brutejudge.__main__:_']
